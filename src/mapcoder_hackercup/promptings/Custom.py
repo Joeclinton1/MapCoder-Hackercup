fc07af365527293dd4364d3a5c5389fb793bce4d
joeclinton1 <joeclinton1@btinternet.com>
2024-09-18 16:50:31 +0100

    Cleanup Custom strategy by extending MapCoder

--- src/mapcoder_hackercup/promptings/Custom.py
+++ src/mapcoder_hackercup/promptings/Custom.py
@@ -1,148 +1,20 @@
 import re
 import xml.etree.ElementTree as ET
 from .Base import BaseStrategy
+from .MapCoder import MapCoder, mapping
 
-
-mapping = {
-    1: "one (01)",
-    2: "two (02)",
-    3: "three (03)",
-    4: "four (04)",
-    5: "five (05)",
-    6: "six (06)",
-    7: "seven (07)",
-    8: "eight (08)",
-    9: "nine (09)",
-}
 
 # KB + Exemplars + Example Planning + Problem Planning + Code Generation + Sample IO testing + Code Improvement
 
-
-class Custom(BaseStrategy):
+class Custom(MapCoder):
     def __init__(
-        self,
-        k: int = 3,
-        t: int = 5,
-        *args,
-        **kwargs
+            self,
+            k: int = 3,
+            t: int = 5,
+            *args,
+            **kwargs
     ):
-        super().__init__(*args, **kwargs)
-        self.k = k
-        self.t = t
-
-    def xml_to_dict(self, element):
-        result = {}
-        for child in element:
-            if child:
-                child_data = self.xml_to_dict(child)
-                if child.tag in result:
-                    if isinstance(result[child.tag], list):
-                        result[child.tag].append(child_data)
-                    else:
-                        result[child.tag] = [result[child.tag], child_data]
-                else:
-                    result[child.tag] = child_data
-            else:
-                result[child.tag] = child.text
-        return result
-
-    def parse_xml(self, response: str) -> dict:
-        if '```xml' in response:
-            response = response.replace('```xml', '')
-        if '```' in response:
-            response = response.replace('```', '')
-
-        try:
-            root = ET.fromstring(response)
-        except:
-            try:
-                root = ET.fromstring('<root>\n' + response + '\n</root>')
-            except:
-                root = ET.fromstring('<root>\n' + response)
-        return self.xml_to_dict(root)
-
-    def parse_code(self, response: str) -> str:
-        if "```" not in response:
-            return response
-
-        code_pattern = r'```((.|\n)*?)```'
-        if "```Python" in response:
-            code_pattern = r'```Python((.|\n)*?)```'
-        if "```Python3" in response:
-            code_pattern = r'```Python3((.|\n)*?)```'
-        if "```python" in response:
-            code_pattern = r'```python((.|\n)*?)```'
-        if "```python3" in response:
-            code_pattern = r'```python3((.|\n)*?)```'
-        if "```C" in response:
-            code_pattern = r'```C((.|\n)*?)```'
-        if "```c" in response:
-            code_pattern = r'```c((.|\n)*?)```'
-        if "```C++" in response:
-            code_pattern = r'```C\+\+((.|\n)*?)```'
-        if "```c++" in response:
-            code_pattern = r'```c\+\+((.|\n)*?)```'
-        if "```Java" in response:
-            code_pattern = r'```Java((.|\n)*?)```'
-        if "```java" in response:
-            code_pattern = r'```java((.|\n)*?)```'
-        if "```Node" in response:
-            code_pattern = r'```Node((.|\n)*?)```'
-        if "```node" in response:
-            code_pattern = r'```node((.|\n)*?)```'
-        if "```Rust" in response:
-            code_pattern = r'```Rust((.|\n)*?)```'
-        if "```rust" in response:
-            code_pattern = r'```rust((.|\n)*?)```'
-        if "```PHP" in response:
-            code_pattern = r'```PHP((.|\n)*?)```'
-        if "```php" in response:
-            code_pattern = r'```php((.|\n)*?)```'
-        if "```Go" in response:
-            code_pattern = r'```Go((.|\n)*?)```'
-        if "```go" in response:
-            code_pattern = r'```go((.|\n)*?)```'
-        if "```Ruby" in response:
-            code_pattern = r'```Ruby((.|\n)*?)```'
-        if "```ruby" in response:
-            code_pattern = r'```ruby((.|\n)*?)```'
-        if "```C#" in response:
-            code_pattern = r'```C#((.|\n)*?)```'
-        if "```c#" in response:
-            code_pattern = r'```c#((.|\n)*?)```'
-        if "```csharp" in response:
-            code_pattern = r'```csharp((.|\n)*?)```'
-
-        code_blocks = re.findall(code_pattern, response, re.DOTALL)
-
-        if type(code_blocks[-1]) == tuple or type(code_blocks[-1]) == list:
-            code_str = "\n".join(code_blocks[-1])
-        elif type(code_blocks[-1]) == str:
-            code_str = code_blocks[-1]
-        else:
-            code_str = response
-
-        return code_str
-
-    @staticmethod
-    def trim_text(text: str, trimmed_text: str):
-        return text.replace(trimmed_text, '').strip()
-
-    @staticmethod
-    def replace_tag(text: str, tag: str):
-        if f'<{tag}><![CDATA[' in text and f']]></{tag}>' in text:
-            return text 
-        else:
-            return text.replace(f'<{tag}>', f'<{tag}><![CDATA[').replace(f'</{tag}>', f']]></{tag}>').strip()
-
-    @staticmethod
-    def get_sample_io_str(sample_io: any) -> str:
-        if len(sample_io) > 0:
-            if type(sample_io[0]) == str:
-                return "\n".join(sample_io)
-            if type(sample_io[0]) == dict:
-                return "\n".join([f"Input:\n{io['input']}\nExpected output:\n{io['output'][0]}" for io in sample_io])
-        return sample_io
+        super().__init__(k, t, *args, **kwargs)
 
     def run_single_pass(self, item: dict):
         print("", flush=True)
@@ -202,9 +74,11 @@ Your response must follow the following xml format-
 
         # Post processing
         response = self.trim_text(
-            response, "# Identify the algorithm (Brute-force, Dynamic Programming, Divide-and-conquer, Greedy, Backtracking, Recursive, Binary search, and so on) that needs to be used to solve the original problem.")
+            response,
+            "# Identify the algorithm (Brute-force, Dynamic Programming, Divide-and-conquer, Greedy, Backtracking, Recursive, Binary search, and so on) that needs to be used to solve the original problem.")
         response = self.trim_text(
-            response, "# Write a useful tutorial about the above mentioned algorithms. Provide a high level generic tutorial for solving this types of problem. Do not generate code.")
+            response,
+            "# Write a useful tutorial about the above mentioned algorithms. Provide a high level generic tutorial for solving this types of problem. Do not generate code.")
         response = self.trim_text(
             response, "# Planning to solve this problem:")
         response = self.trim_text(
@@ -220,7 +94,7 @@ Your response must follow the following xml format-
 
         response = self.parse_xml(response)
 
-        algorithm_prompt = f"## Relevant Algorithm to solve the next problem:\n{ response['algorithm']}"
+        algorithm_prompt = f"## Relevant Algorithm to solve the next problem:\n{response['algorithm']}"
         sample_io_prompt = f"## Sample Test cases: \n{self.get_sample_io_str(item['sample_io'])}\n"
         # if type(self.data) != MBPPDataset and type(self.data) != XCodeDataset else ""
 
