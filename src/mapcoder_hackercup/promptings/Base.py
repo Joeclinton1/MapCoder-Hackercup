5c5cddf137882be25a4592990e6908a0c89ab530
joeclinton1 <joeclinton1@btinternet.com>
2024-09-19 21:48:28 +0100

    Add back Exception handling thing

--- src/mapcoder_hackercup/promptings/Base.py
+++ src/mapcoder_hackercup/promptings/Base.py
@@ -88,13 +88,13 @@ class BaseStrategy(object):
                 cur_imp = ""
 
             while cur_pass < self.pass_at_k and not is_solved:
-                # for _ in range(10):
-                #     try:
-                response, prompt_tokens, completion_tokens = self.run_single_pass(item)
-                #     break
-                # except Exception as e:
-                #     time.sleep(5)
-                #     pass
+                for _ in range(10):
+                    try:
+                        response, prompt_tokens, completion_tokens = self.run_single_pass(item)
+                        break
+                    except Exception as e:
+                        time.sleep(5)
+                        pass
 
                 if response is None:
                     print('No valid code generated!')
