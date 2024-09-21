2ce3193d314c0f8ed16726e61cce0cf6ff22c749
matus <matus.lecky@gmail.com>
2024-09-21 20:01:36 +0100

    update

--- src/mapcoder_hackercup/promptings/Matus.py
+++ src/mapcoder_hackercup/promptings/Matus.py
@@ -14,7 +14,7 @@ class Matus(BaseStrategy):
     def __init__(self, *args, **kwargs):
         self.prompts = utils.load_prompts(prompts_file)
         self.n_plans = kwargs.get('n_plans', 5)
-        self.n_improvements = kwargs.get('n_improvements', 8)
+        self.n_improvements = kwargs.get('n_improvements', 12)
 
         self.pr_tok, self.com_tok = 0, 0
 
@@ -36,17 +36,34 @@ class Matus(BaseStrategy):
 
     def run_single_pass(self, item):
         problem_prompt = self.data.get_prompt(item)
-        planning_prompt = self.prompts['breakdown']['content']\
-            .format(problem_prompt=problem_prompt)
-        sample_io_prompt=f"## Sample Test cases: \n{utils.get_sample_io_str(item['sample_io'])}"
+        sample_io_prompt = f"## Sample Test cases: \n{utils.get_sample_io_str(item['sample_io'])}"
 
         max_score, max_code = 0.0, ""
+        plan_hist = []
 
         for i in range(self.n_plans):
             print(f' --- Attempt {i} --- ')
             print(f' Generating plan ')
 
+            plan_hist_str = ""
+            if plan_hist:
+                plan_hist_str = self.prompts['breakdown']['plan_hist_desc'] + \
+                '\n'.join((self.prompts['breakdown']['separator'].format(idx=idx + 1, plan=plan)
+                    for idx, plan in enumerate(plan_hist)))
+
+            planning_prompt = self.prompts['breakdown']['content'] \
+                .format(problem_prompt=problem_prompt, history=plan_hist_str)
+
             plan = self.chat(planning_prompt, item, 'breakdown')
+            plan_hist.append(
+                self.chat(
+                    self.prompts['breakdown_history']['content']
+                    .format(plan=plan),
+                    item=item,
+                    tag='summary'
+                )
+            )
+
             code_prompt = self.prompts['coding']['content']\
                 .format(problem_prompt=problem_prompt,
                         plan=plan,
@@ -57,6 +74,8 @@ class Matus(BaseStrategy):
             code_output = self.chat(code_prompt, item, tag='code')
             for i in range(self.n_improvements):
                 code = utils.parse_code(code_output)
+                write_debug(code, 'code')
+
                 score, test_result = self.data.evaluate_sample_io(item, code, self.language)
                 print(f' Attempt {i + 1}, score {score}')
 
@@ -70,16 +89,17 @@ class Matus(BaseStrategy):
                     .format(problem_prompt=problem_prompt,
                             code=code,
                             test_log=test_result,
-                            language=self.language)
-                critique = self.chat(critique_prompt, item, 'critique')
-
-                improvement_prompt = self.prompts['improvement']['content'] \
-                    .format(critique=critique,
-                            code=code,
-                            test_log=test_result,
                             std_input_prompt=self.prompts['std_input_prompt']['content'],
                             language=self.language)
+                critique = self.chat(critique_prompt, item, 'code')
 
-                code_output = utils.parse_code(self.chat(improvement_prompt, item, tag='code'))
+                # improvement_prompt = self.prompts['improvement']['content'] \
+                #     .format(critique=critique,
+                #             code=code,
+                #             test_log=test_result,
+                #             std_input_prompt=self.prompts['std_input_prompt']['content'],
+                #             language=self.language)
+
+                code_output = utils.parse_code(critique)  # self.chat(critique, item, tag='code'))
 
         return max_code, self.pr_tok, self.com_tok
