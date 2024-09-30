a0aa62ae4f115959a543023232c2ec7bfed874b3
joeclinton1 <joeclinton1@btinternet.com>
2024-10-01 00:08:38 +0100

    Make pass_at_k run in parallel

--- src/mapcoder_hackercup/promptings/Base.py
+++ src/mapcoder_hackercup/promptings/Base.py
@@ -3,6 +3,9 @@ import tiktoken
 import os
 import copy
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import copy
+import time
 
 from mapcoder_hackercup.models.Base import BaseModel
 from mapcoder_hackercup.datasets.Dataset import Dataset
@@ -52,6 +55,54 @@ class BaseStrategy(object):
         num_items = len(self.data)
         num_success = 0
 
+        def process_pass(item, cur_pass):
+            # The inner loop logic for running a single pass
+            response, prompt_tokens, completion_tokens = None, None, None
+            for _ in range(10):
+                try:
+                    if self.plan is not None:
+                        response, prompt_tokens, completion_tokens = self.run_single_pass_no_planning(item, self.plan)
+                    elif self.code_dir is not None:
+                        result = self.run_single_pass_code_improvement_only(item, self.code_dir)
+                        response, prompt_tokens, completion_tokens = result
+                    else:
+                        response, prompt_tokens, completion_tokens = self.run_single_pass(item)
+                    break
+                except Exception as e:
+                    print(f"Exception occurred with error: {e}")
+                    time.sleep(5)
+
+            if response is None:
+                print('No valid code generated!')
+
+            if hasattr(self, "parse_code"):
+                cur_imp = self.parse_code(response)
+            else:
+                cur_imp = parse_response(response)
+
+            item["source_codes"].append(cur_imp)
+            item["responses"].append(response)
+            item["prompt_tokens"].append(prompt_tokens)
+            item["completion_tokens"].append(completion_tokens)
+            item["no_of_try"] += 1
+
+            if callable(getattr(self.data, "evaluate_sample_io", None)):
+                is_solved_sample, sample_actual_output = self.data.evaluate_sample_io(
+                    item=item,
+                    cur_imp=cur_imp,
+                    language=self.language
+                )
+                item["is_solved_sample"].append(is_solved_sample)
+                item["sample_actual_output"].append(sample_actual_output)
+
+            is_solved, output = self.data.evaluate(
+                item=item,
+                cur_imp=cur_imp,
+                language=self.language
+            )
+
+            return is_solved_sample, is_solved, output, cur_imp
+
         for i, item in enumerate(self.data):
             print("", flush=True, end="")
 
@@ -60,7 +111,6 @@ class BaseStrategy(object):
                 cur_pass = len(item["source_codes"])
                 is_solved = item["is_solved"]
                 output = item["output"]
-                cur_imp = item["source_codes"][-1]
             else:
                 item = copy.deepcopy(item)
                 item["source_codes"] = []
@@ -70,63 +120,32 @@ class BaseStrategy(object):
                 item["no_of_try"] = 0
                 item["is_solved_sample"] = []
                 item["sample_actual_output"] = []
-
                 cur_pass = 0
                 is_solved = False
                 output = ""
-                cur_imp = ""
 
-            while cur_pass < self.pass_at_k and not is_solved:
-                for _ in range(10):
-                    try:
-                        if self.plan is not None:
-                            response, prompt_tokens, completion_tokens = self.run_single_pass_no_planning(item, self.plan)
-                        elif self.code_dir is not None:
-                            result = self.run_single_pass_code_improvement_only(item, self.code_dir)
-                            response, prompt_tokens, completion_tokens = result
-                        else:
-                            response, prompt_tokens, completion_tokens = self.run_single_pass(item)
+            # Using ThreadPoolExecutor to run `pass_at_k` iterations in parallel
+            with ThreadPoolExecutor(max_workers=self.pass_at_k) as executor:
+                future_to_pass = {executor.submit(process_pass, item, cur_pass): cur_pass for _ in
+                                  range(self.pass_at_k)}
+
+                # Loop through completed futures
+                for future in as_completed(future_to_pass):
+                    is_solved_sample, is_solved, output, cur_imp = future.result()
+
+                    # If one of the passes solves the problem, shut down other tasks
+                    if is_solved_sample:
+                        num_success += 1
+
+                        # Cancel all remaining futures
+                        for future_to_cancel in future_to_pass:
+                            if not future_to_cancel.done():
+                                future_to_cancel.cancel()
+
+                        executor.shutdown(wait=False)
                         break
-                    except Exception as e:
-                        print(f"Exception occured with error: {e}")
-                        time.sleep(5)
-                        pass
 
-                if response is None:
-                    print('No valid code generated!')
-
-                if hasattr(self, "parse_code"):
-                    cur_imp = self.parse_code(response)
-                else:
-                    cur_imp = parse_response(response)
-                    # cur_imp = parse_response(response, item.get("entry_point", None))
-
-                item["source_codes"].append(cur_imp)
-                item["responses"].append(response)
-                item["prompt_tokens"].append(prompt_tokens)
-                item["completion_tokens"].append(completion_tokens)
-                item["no_of_try"] += 1
-
-                if callable(getattr(self.data, "evaluate_sample_io", None)):
-                    is_solved_sample, sample_actual_output = self.data.evaluate_sample_io(
-                        item=item,
-                        cur_imp=cur_imp,
-                        language=self.language
-                    )
-                    item["is_solved_sample"].append(is_solved_sample)
-                    item["sample_actual_output"].append(sample_actual_output)
-
-
-                is_solved, output = self.data.evaluate(
-                    item=item,
-                    cur_imp=cur_imp,
-                    language=self.language
-                )
-
-                cur_pass += 1
-
-            if is_solved:
-                num_success += 1
+                    cur_pass += 1
 
             item["is_solved"] = is_solved
             item["language"] = self.language
@@ -145,11 +164,10 @@ class BaseStrategy(object):
 
             if self.verbose:
                 print(
-                    f'completed {i+1}/{num_items}, '
+                    f'completed {i + 1}/{num_items}, '
                     f'Solved: {self.results[i]["is_solved"]},'
-                    f'Solved Sample: {self.results[i]["is_solved_sample"][0]>=0.999}'
+                    f'Solved Sample: {self.results[i]["is_solved_sample"][0] >= 0.999}'
                 )
 
-            # break
-        # output the results to an output folder
+        # Output the results to an output folder
         output_results(self.results.result_path[:-1], self.data)
