55a6605770bba38e6cf626c57c177d8bc5103d56
joeclinton1 <joeclinton1@btinternet.com>
2024-10-16 20:37:34 +0100

    Make hackercup dataset eval match sample

--- src/mapcoder_hackercup/datasets/HackercupDataset.py
+++ src/mapcoder_hackercup/datasets/HackercupDataset.py
@@ -22,7 +22,8 @@ class HackercupDataset(Dataset):
             generated_code=cur_imp,
             id=item[self.id_key],
             tests=item["test_list"],
-            lang=language
+            lang=language,
+            scorer=self.scorer
         )
 
     def evaluate_sample_io(
@@ -30,7 +31,8 @@ class HackercupDataset(Dataset):
         item: dict,
         cur_imp: str,
         language: str,
-        evaluate_on_full_if_passed=True
+        evaluate_on_full_if_passed=True,
+        log_if_passed_samples = False
     ):
 
         results = contest_evaluate_public_tests(
@@ -42,15 +44,22 @@ class HackercupDataset(Dataset):
 
         # evaluate
         if results[0] == 1 and evaluate_on_full_if_passed:
-            results2 = contest_evaluate(
+            if log_if_passed_samples:
+                print("Passed sample input.")
+
+            # Note we overwrite results on purpose so that we have the full outputs for plurarity voting
+            results = contest_evaluate(
                 generated_code=cur_imp,
                 id=item[self.id_key],
                 tests=item["test_list"],
-                lang=language
+                lang=language,
+                scorer=self.scorer
             )
-            if not (isinstance(results2[0], float) or results2[0] is True):
-                return 0.999, f"Program passes Sample Cases, but fails on full input with error: `{results2[1]}`," \
-                              f" Error Type: {results2[0]}"
+
+            if not (isinstance(results[0], float) or results[0] is True):
+                return 0.999, f"Program passes Sample Cases, but fails on full input with error: `{results[1]}`," \
+                              f" Error Type: {results[0]}"
+            return 1.0, results[1]
         return results
 
     @staticmethod
